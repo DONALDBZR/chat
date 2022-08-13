8ad84acf84146fda1659bdded765b754ab1dd540
DONALDBZR <andygaspard@hotmail.com>
2022-08-13 12:50:50 +0400

    [Update 0.2] A new Block

---
+++ Public/Scripts/PHP/Block.php
@@ -0,0 +1,11 @@
+<?php
+// Block class
+class Block
+{
+    // Class variables
+    private string $magicNumber;
+    private int $size;
+    private string $header;
+    private int $transactionCounter;
+    private Transaction $Transaction;
+}
