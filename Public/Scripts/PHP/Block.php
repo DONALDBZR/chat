127e88ec020e84c5b23d8961a0a5b3f14a595531
DONALDBZR <andygaspard@hotmail.com>
2022-08-13 13:43:09 +0400

    [Update 0.5] Defining the characteristics of the block

--- Public/Scripts/PHP/Block.php
+++ Public/Scripts/PHP/Block.php
@@ -9,9 +9,62 @@ class Block
     private string $previousHash;
     private string $nonce;
     // Constructor method
-    public function __construct()
+    protected function __construct()
     {
         // Instantiating Transaction
         $this->Transaction = new Transaction();
+    }
+    // Index accessor method
+    protected function getIndex()
+    {
+        return $this->index;
+    }
+    // Index mutator method
+    protected function setIndex(int $index)
+    {
+        $this->index = $index;
+    }
+    // Transaction accessor method
+    protected function getTransaction()
+    {
+        return $this->Transaction;
+    }
+    // Transaction mutator method
+    protected function setTransaction(Transaction $Transaction)
+    {
+        $this->Transaction = $Transaction;
+    }
+    // Timestamp accessor method
+    protected function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+    // Timestamp mutator method
+    protected function setTimestamp()
+    {
+        // Setting the default timezone
+        date_default_timezone_set("Indian/Mauritius");
+        // Setting the current time to the timestamp
+        $this->timestamp = date("Y-m-d H:i:s");
+    }
+    // Previous Hash accessor method
+    protected function getPreviousHash()
+    {
+        return $this->previousHash;
+    }
+    // Previous Hash mutator method
+    protected function setPreviousHash(string $previousHash)
+    {
+        $this->previousHash = $previousHash;
+    }
+    // Nonce accessor method
+    protected function getNonce()
+    {
+        return $this->nonce;
+    }
+    // Nonce mutator method
+    protected function setNonce(string $nonce)
+    {
+        $this->nonce = $nonce;
     }
 }
