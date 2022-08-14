f363d859558e944fb076ce40f3729529b4c5f945
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 16:43:29 +0400

    [Update 0.8] Route update

--- Public/Scripts/PHP/Router.php
+++ Public/Scripts/PHP/Router.php
@@ -67,23 +67,65 @@ class Router
         }
     }
     // Route method
-    public function route($uri)
+    public function route(string $route, string $path)
     {
-        switch ($uri) {
-            case "":
-            case "/":
-                require_once $_SERVER["DOCUMENT_ROOT"] . "/Views/Homepage.php";
-                break;
-            case "/Login":
-                require_once $_SERVER["DOCUMENT_ROOT"] . "/Views/Login.php";
-                break;
-            case "/Register":
-                require_once $_SERVER["DOCUMENT_ROOT"] . "/Views/Register.php";
-                break;
-            default:
-                http_response_code(404);
-                require_once $_SERVER["DOCUMENT_ROOT"] . "/Views/HTTP404.php";
-                break;
+        // Setting the root of the router
+        $this->setRoot($_SERVER['DOCUMENT_ROOT']);
+        // If-statement to verify that the route is a HTTP 404
+        if ($route === "/404") {
+            // Importing the response for the request
+            require_once "{$this->getRoot()}/{$path}";
+            // Exiting the application
+            exit();
+        } else {
+            // Sanitizing the url
+            $requestUrl = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
+            // Trimming the url
+            $requestUrl = rtrim($requestUrl, '/');
+            // Tokenizing the url
+            $requestUrl = strtok($requestUrl, '?');
+            // Seperating the route
+            $routeParts = explode('/', $route);
+            // Seperating the url
+            $requestUrlParts = explode('/', $requestUrl);
+            // Shifting the elements of the route
+            array_shift($routeParts);
+            // Shifting the elements of the request url
+            array_shift($requestUrlParts);
+            // If-statement to verify that the beginnin gof the route's elements and that the length of the url's elements are 0
+            if ($routeParts[0] === '' && count($requestUrlParts) === 0) {
+                // Importing the response needed
+                require_once "{$this->getRoot()}/{$path}";
+                // Exiting the application
+                exit();
+            } else if (count($routeParts) !== count($requestUrlParts)) {
+                // Initializing the parameters
+                $parameters = array();
+                // For-loop to iterate over the elements of the route
+                for ($index = 0; $index < count($routeParts); $index++) {
+                    // Setting the element at the index positions
+                    $routePart = $routeParts[$index];
+                    // If-statement to verify the route element
+                    if (preg_match("/^[$]/", $routePart)) {
+                        // Trimming the element
+                        $routePart = ltrim($routePart, '$');
+                        // Adding the element of the url at the index's position
+                        array_push($parameters, $requestUrlParts[$index]);
+                        $$routePart = $requestUrlParts[$index];
+                    } else if ($routeParts[$index] !== $requestUrlParts[$index]) {
+                        return;
+                    }
+                }
+            } else if (is_callable($path)) {
+                // Calling the callback function
+                call_user_func($path);
+                exit();
+            } else {
+                // Importing the response
+                require_once "{$this->getRoot()}/{$path}";
+                // Exitting the application
+                exit();
+            }
         }
     }
 }
