ca2054f0d79ef3dbc6d204234301e75e05d51445
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 17:03:33 +0400

    [Update 0.15] Updating the homepage

--- Views/Homepage.php
+++ Views/Homepage.php
@@ -1,4 +1,7 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Public/Scripts/PHP/Router.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Public/Pages/Homepage.html";
-$Router = new Router();
+// Importing Routes
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Routes.php";
+// Importing the required page
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Pages/Homepage.html"; 
+// Instantiating Routes
+$Routes = new Routes();
