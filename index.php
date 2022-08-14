b6ec351da04ed005d9d6a9e23b4c4b77742dc995
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 17:01:13 +0400

    [Update 0.13] Index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Public/Scripts/PHP/Router.php";
-$Router = new Router();
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Routes.php";
+$Router = new Routes();
