2749f08367fa581ebc71b6fa3d1186bbdf8b1f57
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 16:58:26 +0400

    [Update 0.12] Route update

--- Public/Scripts/PHP/Routes.php
+++ Public/Scripts/PHP/Routes.php
@@ -9,5 +9,15 @@ class Routes {
     public function __construct() {
         // Instantiating the Router
         $this->Router = new Router();
+        // Routing the application
+        $this->route();
+    }
+    // Route method
+    public function route()
+    {
+        $this->Router->request("GET", "/", "{$_SERVER['DOCUMENT_ROOT']}/Views/Homepage.php");
+        $this->Router->request("GET", "/Login", "{$_SERVER['DOCUMENT_ROOT']}/Views/Login.php");
+        $this->Router->request("GET", "/Register", "{$_SERVER['DOCUMENT_ROOT']}/Views/Register.php");
+        $this->Router->request("GET", "/404", "{$_SERVER['DOCUMENT_ROOT']}/Views/HTTP404.php");
     }
 }
