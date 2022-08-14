8c42593a83004240f426eb529b5943e1cbd1dc00
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 16:12:29 +0400

    [Update 0.7] Route update

--- Public/Scripts/PHP/Router.php
+++ Public/Scripts/PHP/Router.php
@@ -1,13 +1,72 @@
 <?php
+// Route class
 class Router
 {
+    // Class variables
+    private string $route;
+    private string $path;
+    private string $root;
+    private string $requestMethod;
+    // Constructor method
     public function __construct()
     {
-        $this->initialize();
     }
-    public function __destruct()
+    // Route accessor method
+    public function getRoute()
     {
+        return $this->route;
     }
+    // Route mutator method
+    public function setRoute(string $route)
+    {
+        $this->route = $route;
+    }
+    // Path accessor method
+    public function getPath()
+    {
+        return $this->path;
+    }
+    // Path mutator method
+    public function setPath(string $path)
+    {
+        $this->path = $path;
+    }
+    // Root accessor method
+    public function getRoot()
+    {
+        return $this->root;
+    }
+    // Root mutator method
+    public function setRoot(string $root)
+    {
+        $this->root = $root;
+    }
+    // Request Method accessor method
+    public function getRequestMethod()
+    {
+        return $this->requestMethod;
+    }
+    // Request Method mutator method
+    public function setRequestMethod(string $requestMethod)
+    {
+        $this->requestMethod = $requestMethod;
+    }
+    // Request method
+    public function request(string $requestMethod, string $route, string $path)
+    {
+        // Switch-statement to verify the request method of the route
+        switch ($requestMethod) {
+            case 'GET':
+            case 'POST':
+            case 'PATCH':
+            case 'DELETE':
+            default:
+                // Routing the route
+                $this->route($route, $path);
+                break;
+        }
+    }
+    // Route method
     public function route($uri)
     {
         switch ($uri) {
@@ -26,9 +85,5 @@ class Router
                 require_once $_SERVER["DOCUMENT_ROOT"] . "/Views/HTTP404.php";
                 break;
         }
-    }
-    public function initialize()
-    {
-        $this->route($_SERVER["REQUEST_URI"]);
     }
 }
