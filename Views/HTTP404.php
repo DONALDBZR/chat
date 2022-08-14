5c20a7c098186e7ed61e5e07a0aafcf29b6b7831
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 18:18:02 +0400

    [Update 0.22] Debugging

--- Views/HTTP404.php
+++ Views/HTTP404.php
@@ -1,7 +1,7 @@
 <?php
-// Importing Routes
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Routes.php";
+// Importing Router
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Router.php";
 // Importing the required page
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Pages/HTTP404.html"; 
-// Instantiating Routes
-$Routes = new Routes();
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Pages/HTTP404.html";
+// Instantiating Router
+$Router = new Router();
