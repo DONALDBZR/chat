9373672eb55c3b26f79810d319ba9f1d7bd17436
DONALDBZR <andygaspard@hotmail.com>
2022-08-14 17:04:12 +0400

    [Update 0.16] Updating the 404 response

--- Views/HTTP404.php
+++ Views/HTTP404.php
@@ -1,4 +1,7 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Public/Scripts/php/Router.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/Public/Pages/HTTP404.html";
-$Router = new Router();
+// Importing Routes
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Routes.php";
+// Importing the required page
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Pages/HTTP404.html"; 
+// Instantiating Routes
+$Routes = new Routes();
