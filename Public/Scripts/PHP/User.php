c5c823db8c5eb4388d12928f9ab7f5bdc33a7b56
DONALDBZR <andygaspard@hotmail.com>
2022-08-15 14:33:25 +0400

    [Update 0.37] User.php

--- Public/Scripts/PHP/User.php
+++ Public/Scripts/PHP/User.php
@@ -15,4 +15,34 @@ class User
         // Instantiating PDO
         $this->PDO = new PHPDataObject();
     }
+    // Username accessor method
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    // Username mutator method
+    public function setUsername(string $username)
+    {
+        $this->username = $username;
+    }
+    // Mail Address accessor method
+    public function getMailAddress()
+    {
+        return $this->mailAddress;
+    }
+    // Mail Address mutator method
+    public function setMailAddress(string $mailAddress)
+    {
+        $this->mailAddress = $mailAddress;
+    }
+    // Password accessor method
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    // Password mutator method
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
 }
