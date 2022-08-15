73fa41d529fff080bbc91f65f3b5ac5e2eaf6b40
DONALDBZR <andygaspard@hotmail.com>
2022-08-15 14:29:14 +0400

    [Update 0.35] User.php

---
+++ Public/Scripts/PHP/User.php
@@ -0,0 +1,11 @@
+<?php
+// Importing PDO
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/PDO.php";
+// User class
+class User
+{
+    // Class variables
+    private string $username;
+    private string $mailAddress;
+    private string $password;
+}
