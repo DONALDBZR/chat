e6acb70873ecf0f1183fc165483313c9184c403c
DONALDBZR <andygaspard@hotmail.com>
2022-08-22 12:13:42 +0400

    [Update 0.85] Debugging

--- Views/UserDashboard.php
+++ Views/UserDashboard.php
@@ -1,9 +1,23 @@
 <?php
 // Importing Routes
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
-// Importing the required page
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Pages/UserDashboard.html";
-// Setting the session variable for the front-end to retrieve it
-$User = $_SESSION['User'];
-header('Content-Type: application/json');
-echo json_encode($User);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/Public/Stylesheets/chat.css" />
+    <script src="/Modules/React/React - Production - Minified - 18.3.0.js"></script>
+    <script src="/Modules/React/ReactDOM - Production - Minified - 18.3.0.js"></script>
+    <script src="/Modules/React/Babel - Minified - 6.26.0.js"></script>
+    <title><?php echo $_SESSION['User']['username'] ?></title>
+</head>
+
+<body id="userDashboard">
+    <script src="/Public/Scripts/JS/UserDashboard.js" type="text/babel"></script>
+</body>
+
+</html>
