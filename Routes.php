5fa50f009bddc8f239771f8c463634f882128a09
DONALDBZR <andygaspard@hotmail.com>
2022-08-22 11:15:36 +0400

    [Update 0.80] Debugging

--- Routes.php
+++ Routes.php
@@ -4,23 +4,33 @@ require_once "{$_SERVER['DOCUMENT_ROOT']}/Router.php";
 // Switch-statement to verify the request method
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        // If-statement to verify the url
-        if ($_SERVER['REQUEST_URI'] == '/') {
-            $Router = new Router("GET", "/", "/Views/Homepage.php");
-        } else if ($_SERVER['REQUEST_URI'] == '/Login') {
-            $Router = new Router("GET", "/Login", "/Views/Login.php");
-        } else if ($_SERVER['REQUEST_URI'] == '/Register') {
-            $Router = new Router("GET", "/Register", "/Views/Register.php");
-        } else if (str_contains($_SERVER['REQUEST_URI'], '/User/Dashboard')) {
-            $Router = new Router("GET", "/User/Dashboard", "/Views/UserDashboard.php");
+        // Switch-statement to verify the url
+        switch ($_SERVER['REQUEST_URI']) {
+            case '/':
+                $Router = new Router("GET", "/", "/Views/Homepage.php");
+                break;
+            case '/Login':
+                $Router = new Router("GET", "/Login", "/Views/Login.php");
+                break;
+            case '/Register':
+                $Router = new Router("GET", "/Register", "/Views/Register.php");
+                break;
+            case '/User':
+                if (str_contains($_SERVER['REQUEST_URI'], '/User/Dashboard')) {
+                    $Router = new Router("GET", "/User/Dashboard", "/Views/UserDashboard.php");
+                }
+                break;
         }
         break;
     case 'POST':
-        // If-statement to verify the url
-        if ($_SERVER['REQUEST_URI'] == '/Login') {
-            $Router = new Router("POST", "/Login", "/Controllers/Login.php");
-        } else if ($_SERVER['REQUEST_URI'] == '/Register') {
-            $Router = new Router("POST", "/Register", "/Controllers/Register.php");
+        // Switch-statement to verify the url
+        switch ($_SERVER['REQUEST_URI']) {
+            case '/Login':
+                $Router = new Router("POST", "/Login", "/Controllers/Login.php");
+                break;
+            case '/Register':
+                $Router = new Router("POST", "/Register", "/Controllers/Register.php");
+                break;
         }
         break;
 }
