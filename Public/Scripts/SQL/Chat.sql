f26613e87620f74a4a616ea74dc41e3fd23b8916
DONALDBZR <andygaspard@hotmail.com>
2022-08-23 14:06:40 +0400

    [Update 0.2.1] Creating the Table for the Login Tracking System

--- Public/Scripts/SQL/Chat.sql
+++ Public/Scripts/SQL/Chat.sql
@@ -6,3 +6,11 @@ CREATE TABLE Chat.Users (
     UsersMailAddress VARCHAR(64),
     UsersPassword VARCHAR(256)
 );
+-- Creating the Logins table
+CREATE TABLE Chat.Logins (
+    LoginsId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    LoginsUser VARCHAR(32),
+    LoginsTimeIn VARCHAR(32),
+    LoginsTimeOut VARCHAR(32),
+    CONSTRAINT fkLoginsUsers FOREIGN KEY (LoginsUser) REFERENCES Chat.Users (UsersUsername)
+);
