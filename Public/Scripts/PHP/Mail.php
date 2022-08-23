070bd43a0ecb437ee9c360f300f35cad65ddbc18
DONALDBZR <andygaspard@hotmail.com>
2022-08-23 20:24:21 +0400

    [Update 0.3.2] Implementing the mailing service

--- Public/Scripts/PHP/Mail.php
+++ Public/Scripts/PHP/Mail.php
@@ -34,6 +34,59 @@ class Mail
     public function __construct()
     {
         $this->PHPMailer = new PHPMailer(true);
-        $this->configure();
+    }
+    // Recipient accessor method
+    public function getRecipient()
+    {
+        return $this->recipient;
+    }
+    // Recipient mutator method
+    public function setRecipient(string $recipient)
+    {
+        $this->recipient = $recipient;
+    }
+    // Subject accessor method
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+    // Subject mutator method
+    public function setSubject(string $subject)
+    {
+        $this->subject = $subject;
+    }
+    // Message accessor method
+    public function getMessage()
+    {
+        return $this->message;
+    }
+    // Message mutator method
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+    }
+    /**
+     * Sending the mail after having configured PHPMailer
+     */
+    public function send(string $recipient, string $subject, string $message)
+    {
+        $this->setRecipient($recipient);
+        $this->setSubject($subject);
+        $this->setMessage($message);
+        $this->PHPMailer->isSMTP();
+        $this->PHPMailer->CharSet = "UTF-8";
+        $this->PHPMailer->Host = "smtp-mail.outlook.com";
+        $this->PHPMailer->SMTPDebug = 0;
+        $this->PHPMailer->Port = 587;
+        $this->PHPMailer->SMTPSecure = 'tls';
+        $this->PHPMailer->SMTPAuth = true;
+        $this->PHPMailer->isHTML(true);
+        $this->PHPMailer->Username = Environment::MailUsername;
+        $this->PHPMailer->Password = Environment::MailPassword;
+        $this->PHPMailer->setFrom($this->PHPMailer->Username);
+        $this->PHPMailer->addAddress($this->getRecipient());
+        $this->PHPMailer->Subject = $this->getSubject();
+        $this->PHPMailer->Body = $this->getMessage();
+        $this->PHPMailer->send();
     }
 }
