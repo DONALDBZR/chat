9951345c581989e0ec9f7e4b1fbc18f780e6d247
DONALDBZR <andygaspard@hotmail.com>
2022-08-23 21:07:31 +0400

    [Update 0.4.7] Improving the login process

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -3,21 +3,19 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 // Importing User
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/User.php";
-// Importing Login
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Login.php";
-// Instantiating Login
-$Login = new Login();
+// Instantiating User
+$User = new User();
 // If-statement to verify that there is a json
 if (json_decode(file_get_contents("php://input")) != null) {
     // If-statement to verify that the JSON does not have any null value
     if (!empty(json_decode(file_get_contents("php://input"))->username) && !empty(json_decode(file_get_contents("php://input"))->password)) {
         // Starting the login process
-        $Login->trackIn();
+        $User->login();
     } else {
         // JSON to be encoded and sent to the client
         $json = array(
             "success" => "failure",
-            "url" => $Login->domain . "/Login",
+            "url" => $User->domain . "/Login",
             "message" => "The form must be completely filled!"
         );
         // Preparing the header for the JSON
@@ -29,7 +27,7 @@ if (json_decode(file_get_contents("php://input")) != null) {
     // JSON to be encoded and sent to the client
     $json = array(
         "success" => "failure",
-        "url" => $Login->domain . "/Login",
+        "url" => $User->domain . "/Login",
         "message" => "The form must be completely filled!"
     );
     // Preparing the header for the JSON
