f528905dbddaa27a6de5a9dc216b7176f2d8b51b
DONALDBZR <andygaspard@hotmail.com>
2022-09-01 15:52:33 +0400

    [Update 0.4.42] Debugging

--- Public/Scripts/JS/HTTP404.js
+++ Public/Scripts/JS/HTTP404.js
@@ -2,6 +2,35 @@
  * The application that is going to be rendered by React.js.
  */
 class Application extends React.Component {
+    /**
+     * Adding the main stylesheet to the Application
+     */
+    mainStylesheet() {
+        const link = document.createElement("link");
+        link.href = "/Public/Stylesheets/chat.css";
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        document.head.appendChild(link);
+    }
+    /**
+     * Adding the media queries stylesheets to the Application
+     */
+    mediaQueriesStylesheets() {
+        const stylesheets = ["/Public/Stylesheets/desktop.css", "/Public/Stylesheets/tablet.css", "/Public/Stylesheets/mobile.css"];
+        const mediaQueries = ["screen and (min-width: 1024px)", "screen and (min-width: 640px) and (max-width: 1023px)", "screen and (max-width: 639px)"];
+        for (let index = 0; index < stylesheets.length; index++) {
+            const link = document.createElement("link");
+            link.href = stylesheets[index];
+            link.media = mediaQueries[index];
+            link.rel = "stylesheet";
+            link.type = "text/css";
+            document.head.appendChild(link);
+        }
+    }
+    componentDidMount() {
+        this.mainStylesheet();
+        this.mediaQueriesStylesheets();
+    }
     render() {
         return <Header />;
     }
