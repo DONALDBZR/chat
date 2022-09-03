2ed477bbbce0773af6f64a4eb1b07e278a3ad6d0
DONALDBZR <andygaspard@hotmail.com>
2022-09-03 11:28:48 +0400

    [Update 0.5.14] Debugging

--- Controllers/User.php
+++ Controllers/User.php
@@ -1,4 +1,6 @@
 <?php
+// Importing Routes
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 // Setting the session variable for the front-end to retrieve it
 $User = $_SESSION['User'];
 header('Content-Type: application/json');
