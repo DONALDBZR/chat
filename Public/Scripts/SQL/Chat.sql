b88ef3fc67d9780dadd7b548bf9f77f49c2e89ca
DONALDBZR <andygaspard@hotmail.com>
2022-09-03 09:27:35 +0400

    [Update 0.5.7] Creation of the Contacts Table

--- Public/Scripts/SQL/Chat.sql
+++ Public/Scripts/SQL/Chat.sql
@@ -8,9 +8,17 @@ CREATE TABLE Chat.Users (
 );
 -- Creating the Logins table
 CREATE TABLE Chat.Logins (
-    LoginsId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    LoginsId INT PRIMARY KEY AUTO_INCREMENT,
     LoginsUser VARCHAR(32),
     LoginsTimeIn VARCHAR(32),
     LoginsTimeOut VARCHAR(32),
     CONSTRAINT fkLoginsUsers FOREIGN KEY (LoginsUser) REFERENCES Chat.Users (UsersUsername)
+);
+-- Creating the Contacts table
+CREATE TABLE Chat.Contacts (
+    ContactsId INT PRIMARY KEY AUTO_INCREMENT,
+    ContactsUser VARCHAR(32),
+    ContactsFriends VARCHAR(32),
+    CONSTRAINT fkContactsUserUsersUsername FOREIGN KEY (ContactsUser) REFERENCES Chat.Users (UsersUsername),
+    CONSTRAINT fkContactsFriendsUsersUsername FOREIGN KEY (ContactsFriends) REFERENCES Chat.Users (UsersUsername)
 );
