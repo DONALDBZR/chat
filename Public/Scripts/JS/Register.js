0f67155e7311c9959a27439fef0da40606d2099a
DONALDBZR <andygaspard@hotmail.com>
2022-09-30 21:42:09 +0400

    [UPDATE 0.11.6] Optimizing the application

--- Public/Scripts/JS/Register.js
+++ Public/Scripts/JS/Register.js
@@ -169,4 +169,4 @@ class Footer extends Application {
     }
 }
 // Rendering page
-ReactDOM.render(<Application />, document.getElementById("register"));
+ReactDOM.render(<Application />, document.getElementById("Register"));
